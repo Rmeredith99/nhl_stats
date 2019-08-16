293f743c7a9868286572d75bea9be8f39fda38eb
Ryan Meredith <rcm269@cornell.edu>
2019-08-15 17:46:42 -0700

    Started to add code to create stat models

--- stats/views.py
+++ stats/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    """
+    The first page of the stats app. Not actually called 'home'
+        in the url
+    """
+    return render(request, 'stats/home.html', {})
