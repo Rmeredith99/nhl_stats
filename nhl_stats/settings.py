98a8849d6ee7ffbfea06622f499af46aa1977391
Ryan Meredith <rcm269@cornell.edu>
2019-09-25 23:22:14 -0700

    Changed allowed hosts

--- nhl_stats/settings.py
+++ nhl_stats/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '172.31.20.71']
+ALLOWED_HOSTS = ['127.0.0.1', '172.31.20.71', "localhost"]
 
 
 # Application definition
